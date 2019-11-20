28566d7a556c24225bb72b0060a38cc5ac42c226
Tomas <finixit@email.cz>
2019-11-21 00:20:51 +0100

    create Image

--- src/ImageManager.php
+++ src/ImageManager.php
@@ -2,5 +2,25 @@
 
 namespace Tomsq\ImageManager;
 
+use Intervention\Image as InterventionImage;
+use Illuminate\Support\Facades\Storage;
+
+
 class ImageManager
-{ }
+{
+    public static function createImage($file, $options = [])
+    {
+        return InterventionImage::make($file)->resize(1080, null, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        })->stream(
+            'jpg',
+            array_key_exists("compresion", $options) ? $options["compresion"] : 90
+        );
+    }
+
+    public static function saveImage(InterventionImage $image)
+    {
+        dd($image);
+    }
+}
--- src/Traits/HasImages.php
+++ src/Traits/HasImages.php
@@ -3,6 +3,7 @@
 namespace Tomsq\ImageManager\Traits;
 
 use Tomsq\ImageManager\Models\Images\Image;
+use Tomsq\ImageManager\ImageManager;
 
 trait HasImages
 {
@@ -11,12 +12,26 @@ trait HasImages
         return $this->morphMany(Image::class, 'model');
     }
 
-    public function createImage()
+    /**
+     *  @param string $file - encoded file
+     *  @param array $data - additional image information
+     *  @param array options
+     *
+     *  @return Tomsq\ImageManager\Models\Images\Image
+     */
+    public function createImage($file, array $data, array $options = [])
     {
+        $interventionImage = ImageManager::createImage($file, $options);
+        $storedData = ImageManager::saveImage($interventionImage, $options);
+
         return $this->images()->create([
-            'file_name' => 'test',
-            'file_extension' => 'png',
-            'file_size' => 1024
+            'file_name' => $storedData['file_name'],
+            'file_extension' => $storedData['file_extension'],
+            'file_size' => $storedData['file_size'],
+            'created_by' => array_key_exists('created_by', $data) ? $data['created_by'] : null,
+            'short_description' => array_key_exists('short_description', $data) ? $data['short_description'] : null,
+            'description' => array_key_exists('description', $data) ? $data['description'] : null,
+            'name' => array_key_exists('short_description', $data) ? $data['name'] : null
         ]);
     }
 
