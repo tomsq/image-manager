319849dbce887dee91933cbff580433d3777e7f6
Tomas <finixit@email.cz>
2019-11-22 12:56:00 +0100

    config cleaned

--- src/config/image-manager.php
+++ src/config/image-manager.php
@@ -2,27 +2,4 @@
 
 return [
 
-    'images' => [
-        'mainImage' => [
-            'width' => 1080,
-            'height' => null,
-            'file_extension' => 'jpg',
-            'compression' => 95
-        ],
-
-        'thumbImage' => [
-            'width' => 400,
-            'height' => null,
-            'file_extension' => 'jpg',
-            'compression' => 85
-        ],
-
-        'skeleton' => [
-            'width' => 400,
-            'height' => null,
-            'file_extension' => 'jpg',
-            'compression' => 5
-        ]
-    ]
-
 ];
