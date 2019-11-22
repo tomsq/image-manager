2b0a3b22e13daf4cc31c0abbccda2e1e4eba7aa9
Tomas <finixit@email.cz>
2019-11-22 12:59:57 +0100

    Migrated from config version to options

--- src/ImageManager.php
+++ src/ImageManager.php
@@ -8,54 +8,74 @@ use Tomsq\ImageManager\Models\Images\Image;
 
 class ImageManager
 {
-    public static function handleImage($file, $options = [])
+    public static function handleImage($file, $model, $imageData, $options = [])
     {
-        // dd(config('image-manager.images'));
-        $imageTypesConfigs = config('image-manager.images');
-        $pathBase = uniqid() . '/';
-        foreach($imageTypesConfigs as $imageTypeName => $config)
-        {
-            $img = self::makeImage($file, $config);
-            $filePath = self::makeFilePath($pathBase, $imageTypeName, $config);
-            self::saveImage($filePath, $img);
-            self::createModel($filePath, $imageTypeName, $config);
-        }
+        $dirName = uniqid();
+        $mainImageModel = self::handleMainImage($file, $dirName, $model, $imageData, $options);
+        self::handleThumbImage($file, $dirName, $model, $imageData, $mainImageModel->id, $options);
     }
 
-    private static function makeImage($file, $config)
+    private static function handleMainImage($file, $dirName, $model, $imageData, $options = [])
+    {
+        $img = self::makeMainImage($file, $options);
+        $path = new FilePath($dirName, 'main', 'jpg');
+        self::saveImage($path->getFullPath(), $img);
+        return self::createModel($model, $imageData, $path);
+    }
+
+    private static function handleThumbImage($file, $dirName, $model, $imageData, $mainImageModelId, $options = [])
+    {
+        $img = self::makeThumbImage($file, $options);
+        $path = new FilePath($dirName, 'thumb', 'jpg');
+        self::saveImage($path->getFullPath(), $img);
+        self::createModel($model, $imageData, $path, $mainImageModelId);
+    }
+
+    private static function makeMainImage($file, $options)
     {
         return InterventionImage::make($file)->resize(
-            $config['width'],
-            $config['height'],
+            1080,
+            null,
             function ($constraint) {
                 $constraint->aspectRatio();
                 $constraint->upsize();
             })->stream(
-                $config['file_extension'],
-                $config['compression']
+                'jpg',
+                90
             );
     }
 
-    private static function createModel($filePath, $imageTypeName, $fileSize, $config)
+    private static function makeThumbImage($file, $options)
     {
-        Image::create([
-            'file_name' => $filePath,
-            'file_extension' => $config['file_extension'],
-            'type' => $imageTypeName,
-            // 'created_by' => array_key_exists('created_by', $data) ? $data['created_by'] : null,
-            // 'short_description' => array_key_exists('short_description', $data) ? $data['short_description'] : null,
-            // 'description' => array_key_exists('description', $data) ? $data['description'] : null,
-            // 'name' => array_key_exists('short_description', $data) ? $data['name'] : null
+        return InterventionImage::make($file)->resize(
+            400,
+            null,
+            function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            })->stream(
+                'jpg',
+                85
+            );
+    }
+
+    private static function createModel($model, $imageData, FilePath $path, $parentId = null)
+    {
+        return $model->images()->create([
+            'name' => array_key_exists('short_description', $imageData) ? $imageData['name'] : null,
+            'description' => array_key_exists('description', $imageData) ? $imageData['description'] : null,
+            'short_description' => array_key_exists('short_description', $imageData) ? $imageData['short_description'] : null,
+            'created_by' => array_key_exists('created_by', $imageData) ? $imageData['created_by'] : null,
+            'file_name' => $path->getFileName(),
+            'file_extension' => $path->getExtension(),
+            'file_size' => Storage::size('public/' . $path->getFullPath()),
+            'parent_id' => $parentId,
+            'type' => $path->getImageType()
         ]);
     }
 
     private static function saveImage($imagePath, $image)
     {
         Storage::disk('public')->put($imagePath, $image);
-    }
-
-    private static function makeFilePath($pathBase, $typeName, $config)
-    {
-        return $pathBase . $typeName . '.' . $config['file_extension'];
     }
 }
