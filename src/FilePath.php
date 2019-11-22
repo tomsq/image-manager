16c6e0f370d96c9c17c9288794a8cfabe862e3d1
Tomas <finixit@email.cz>
2019-11-22 12:55:03 +0100

    File Path class added

---
+++ src/FilePath.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tomsq\ImageManager;
+
+class FilePath
+{
+    protected $extension;
+    protected $dirName;
+    protected $imageType;
+
+    function __construct($dirName, $imageType, $extension)
+    {
+        $this->dirName = $dirName;
+        $this->imageType = $imageType;
+        $this->extension = $extension;
+    }
+
+    public function getFullPath()
+    {
+        return $this->dirName . '/' .$this->imageType . '.' . $this->extension;
+    }
+
+    public function getFileName()
+    {
+        return $this->dirName . '/' . $this->imageType;
+    }
+
+    public function getDirName()
+    {
+        return $this->dirName;
+    }
+
+    public function getImageType()
+    {
+        return $this->imageType;
+    }
+
+    public function getExtension()
+    {
+        return $this->extension;
+    }
+}
